b317c1bf255ab1400d070168b8064e73173a2f87
Vaneza Aumento <vaumento@olinkgroup.com>
2017-02-28 19:32:42 +0800

    added bootstrap files

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -6,7 +6,9 @@
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/bootstrap.css">
+    <?php wp_head(); ?>
+    <script src="<?php echo get_template_directory_uri(); ?>../js/bootstrap.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>../js/main.js"></script>
 
 		<?php wp_footer(); ?>
--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,39 +1,39 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/style.css">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/responsive.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>"
-
-
-		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>
+        <?php wp_title(''); ?>
+            <?php if(wp_title('', false)) { echo ' :'; } ?>
+                <?php bloginfo('name'); ?>
+    </title>
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/responsive.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>../css/bootstrap.css">
+    <?php wp_head(); ?>
+        <script>
+            // conditionizr.com
+            // configure environment tests
+            conditionizr.config({
+                assets: '<?php echo get_template_directory_uri(); ?>'
+                , tests: {}
+            });
         </script>
-	</head>
-    <body <?php body_class(); ?>>
+</head>
+
+<body <?php body_class(); ?>>
     <div class="header">
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                    <a class="navbar-brand image-responsive" href="#home"><img src="<?php echo get_template_directory_uri(); ?>../image/logo.png"></a>
-                </div>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand image-responsive" href="#home"><img src="<?php echo get_template_directory_uri(); ?>../image/logo.png"></a> </div>
                 <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#start">START</a></li>
@@ -47,4 +47,3 @@
             <!--/.container-fluid -->
         </nav>
     </div>
-
